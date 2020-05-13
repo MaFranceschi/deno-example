a2c19fd751b62f54e75b0dfbd4d886e296d12ba9
Marco Franceschi <mfranceschi@talpor.com>
2020-05-13 00:56:52 -0400

    Created get publications endpoint

--- routes/publications.ts
+++ routes/publications.ts
@@ -1,13 +1,15 @@
 import { Router } from "https://deno.land/x/oak/mod.ts";
 import PublicationService from "../services/publication.ts";
-import { get } from "../util/storage.ts";
+import { Publication } from "../types/types.ts";
 
 const router = new Router();
 
 router.get("/publications", async ({ response }) => {
-  const profile = await PublicationService.getPublications();
-  if (profile) {
-    response.body = { message: "success", data: profile };
+  const publications: Publication[] | undefined = await PublicationService
+    .getPublications();
+
+  if (publications) {
+    response.body = { message: "success", data: publications };
   } else {
     response.body = { message: "an error ocurred" };
   }
--- services/publication.ts
+++ services/publication.ts
@@ -1,9 +1,10 @@
 import * as log from "https://deno.land/std/log/mod.ts";
 import { integrationKey, mediumUrl } from "../util/environment.ts";
+import { Publication } from "../types/types.ts";
 import { get } from "../util/storage.ts";
 
 const PublicationService = {
-  getPublications: async () => {
+  getPublications: async (): Promise<Publication[] | undefined> => {
     try {
       const { id: userId } = get();
       const response = await fetch(
@@ -15,10 +16,10 @@ const PublicationService = {
         },
       );
       const { data } = await response.json();
+      log.debug("publications retrieved");
       return data;
     } catch (error) {
       log.error(error);
-      return null;
     }
   },
 };
