2ec7654bcab70cbe5e82f88feb5ac0171937ac46
Marco Franceschi <mfranceschi@talpor.com>
2020-05-13 00:57:30 -0400

    Added types for endpoints

--- types/types.ts
+++ types/types.ts
@@ -13,3 +13,12 @@ export interface Publication {
   url: string;
   imageUrl: string;
 }
+
+export interface Post {
+  title: string;
+  contentFormat: string;
+  content: string;
+  canonicalUrl: string;
+  tags: string;
+  publishStatus: string;
+}
